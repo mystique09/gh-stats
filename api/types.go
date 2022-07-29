2a0d9f474eced2f84e0c1ed45825b258d4ce8105
mystique09 <knightkrypto7@gmail.com>
2022-07-30 01:55:50 +0800

    add: custom httpresponse type for channel

--- api/types.go
+++ api/types.go
@@ -1,7 +1,14 @@
 package api
 
+import "net/http"
+
 type GithunUserInfo struct {
 	Id       uint32 `json:"id"`
 	Username string `json:"login"`
 	NodeId   string `json:"node_id"`
+}
+
+type HttpResponse struct {
+	response *http.Response
+	err      error
 }
