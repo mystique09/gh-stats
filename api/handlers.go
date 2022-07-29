5d6ba5ad3314d9ee8c6a13ad72d44e49f6cd774d
mystique09 <knightkrypto7@gmail.com>
2022-07-30 01:56:01 +0800

    feat: label, and channels

--- api/handlers.go
+++ api/handlers.go
@@ -11,22 +11,32 @@ import (
 func profileVisits(c echo.Context) error {
 	name := c.Param("name")
 	color := c.QueryParam("color")
+	label := c.QueryParam("label")
 
 	if color == "" {
 		color = badge.ColorBlue.String()
 	}
 
-	var resp *http.Response
+	if label == "" {
+		label = "Profile Visits"
+	}
 
-	resp, err := http.Get("https://api.github.com/users/" + name)
-	if err != nil || resp.StatusCode == 404 {
+	resp := make(chan *HttpResponse)
+
+	go func() {
+		r, e := http.Get("https://api.github.com/users/" + name)
+		resp <- &HttpResponse{response: r, err: e}
+	}()
+
+	recv := <-resp
+	if recv.err != nil || recv.response.StatusCode == 404 {
 		badge, err := badge.RenderBytes("User not found", "0", "red")
 		if err != nil {
 			return c.String(400, err.Error())
 		}
 		return c.Blob(404, "image/svg+xml", badge)
 	}
-	respData, err := ioutil.ReadAll(resp.Body)
+	respData, err := ioutil.ReadAll(recv.response.Body)
 	if err != nil {
 		return c.String(400, err.Error())
 	}
@@ -34,7 +44,7 @@ func profileVisits(c echo.Context) error {
 	var userInfo GithunUserInfo
 	json.Unmarshal(respData, &userInfo)
 
-	badge, err := badge.RenderBytes("profile-visits", "0", badge.Color(color))
+	badge, err := badge.RenderBytes(label, "0", badge.Color(color))
 	if err != nil {
 		return c.String(400, err.Error())
 	}
