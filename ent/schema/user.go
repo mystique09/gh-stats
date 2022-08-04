02017bad9d9101406916204b95af0b56a377aa29
mystique09 <knightkrypto7@gmail.com>
2022-08-05 01:06:24 +0800

    add: user schema and generated schema

---
+++ ent/schema/user.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Uint32("id"),
+		field.String("node_id"),
+		field.String("username").NotEmpty(),
+		field.Uint64("visits").Default(0),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
