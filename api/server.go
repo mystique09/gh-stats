fd097fba63d54365094608ab897739ed41c1b351
mystique09 <knightkrypto7@gmail.com>
2022-08-05 01:06:57 +0800

    feat: add create db and update db

--- api/server.go
+++ api/server.go
@@ -1,17 +1,37 @@
 package api
 
 import (
+	"context"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/mystique09/gh-profile/ent"
 )
 
 func Launch() {
+	godotenv.Load()
+
+	client, err := ent.Open("postgres", os.Getenv("DB_DB"))
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	server := Server{
+		db: client,
+	}
 	app := echo.New()
+
 	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
 	}))
-	app.GET("/profile/:name", profileVisits)
+	app.GET("/profile/:name", server.profileVisits)
 	log.Fatal(app.Start(":3000"))
 }
