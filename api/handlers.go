3f8b618f0131164ed650ab5d139d150bcc6684bf
mystique09 <knightkrypto7@gmail.com>
2022-08-05 01:06:48 +0800

    feat: init db

--- api/handlers.go
+++ api/handlers.go
@@ -2,13 +2,25 @@ package api
 
 import (
 	"encoding/json"
-	"github.com/labstack/echo/v4"
-	"github.com/narqo/go-badge"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+	"github.com/mystique09/gh-profile/ent/user"
+	"github.com/narqo/go-badge"
 )
 
-func profileVisits(c echo.Context) error {
+func renderErrorBadge(c echo.Context, label string) error {
+	badge, err := badge.RenderBytes(label, "0", "red")
+	if err != nil {
+		return c.String(400, err.Error())
+	}
+	return c.Blob(404, "image/svg+xml", badge)
+}
+
+func (s *Server) profileVisits(c echo.Context) error {
 	name := c.Param("name")
 	color := c.QueryParam("color")
 	label := c.QueryParam("label")
@@ -30,11 +42,8 @@ func profileVisits(c echo.Context) error {
 
 	recv := <-resp
 	if recv.err != nil || recv.response.StatusCode == 404 {
-		badge, err := badge.RenderBytes("User not found", "0", "red")
-		if err != nil {
-			return c.String(400, err.Error())
-		}
-		return c.Blob(404, "image/svg+xml", badge)
+		renderErrorBadge(c, "User not found.")
+		return nil
 	}
 	respData, err := ioutil.ReadAll(recv.response.Body)
 	if err != nil {
@@ -44,7 +53,18 @@ func profileVisits(c echo.Context) error {
 	var userInfo GithunUserInfo
 	json.Unmarshal(respData, &userInfo)
 
-	badge, err := badge.RenderBytes(label, "0", badge.Color(color))
+	update_user, err := s.db.User.Update().Where(user.ID(userInfo.Id), user.NodeID(userInfo.NodeId), user.Username(userInfo.Username)).AddVisits(1).Save(c.Request().Context())
+	if err != nil || update_user == 0 {
+		s.db.User.Create().SetID(userInfo.Id).SetNodeID(userInfo.NodeId).SetUsername(userInfo.Username).SetVisits(0).Save(c.Request().Context())
+		badge, _ := badge.RenderBytes(label, "0", badge.Color(color))
+		return c.Blob(200, "image/svg+xml", badge)
+	}
+
+	get_user, err := s.db.User.Get(c.Request().Context(), userInfo.Id)
+	if err != nil {
+		return c.String(400, err.Error())
+	}
+	badge, err := badge.RenderBytes(label, strconv.Itoa(int(get_user.Visits)), badge.Color(color))
 	if err != nil {
 		return c.String(400, err.Error())
 	}
