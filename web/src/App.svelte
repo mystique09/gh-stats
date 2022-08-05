49f85119f90094c4016253a7b97eb5537cfbba70
mystique09 <knightkrypto7@gmail.com>
2022-08-05 08:33:18 +0800

    feat: testing

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -3,4 +3,9 @@
 
 <main class="bg-gray-300">
   <h1 class="text-gray-400">Hello, World!</h1>
+  <p>
+    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum
+    sint consectetur cupidatat.
+  </p>
+  <a href="/">Profile Visits</a>
 </main>
