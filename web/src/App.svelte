fc6caf5160c4ff12d96842c48fa977ef067d8107
mystique09 <knightkrypto7@gmail.com>
2022-08-05 09:26:02 +0800

    Revert "feat: testing"
    This reverts commit 49f85119f90094c4016253a7b97eb5537cfbba70.

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -3,9 +3,4 @@
 
 <main class="bg-gray-300">
   <h1 class="text-gray-400">Hello, World!</h1>
-  <p>
-    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum
-    sint consectetur cupidatat.
-  </p>
-  <a href="/">Profile Visits</a>
 </main>
