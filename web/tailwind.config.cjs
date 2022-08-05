9aff995630040e691cd563d0152def515c494243
mystique09 <knightkrypto7@gmail.com>
2022-08-05 13:52:47 +0800

    feat: tailwindcss

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
     "./src/**/*.{svelte,html,js,ts}",
   ],
   theme: {
+    colors: {
+      base: "#002B5B",
+      primary: "#2B4865",
+      secondary: "#256D85",
+      accent: "#8FE3CF",
+      white: '#FFFFFF',
+      gray: '#8492a6',
+    },
     extend: {},
   },
   plugins: [],
