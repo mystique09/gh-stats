9e35bd592e5691eace3b0bc0512c93f11ae6a49e
mystique09 <knightkrypto7@gmail.com>
2022-08-05 13:51:57 +0800

    add: navbar

---
+++ web/src/lib/Navbar.svelte
@@ -0,0 +1,49 @@
+<script>
+  import githubIcon from "../assets/github-svgrepo-com.svg";
+  const GITHUB_URL = "https://github.com/mystique09/gh-stats";
+</script>
+
+<div class="wrap">
+  <nav>
+    <h2><a href="/">{"<Github Status />"}</a></h2>
+    <ul>
+      <li>
+        <a class="github_img" href={GITHUB_URL}>
+          <img
+            rel="opener"
+            target="_blank"
+            referrerpolicy="noreferrer"
+            class="github_icon"
+            src={githubIcon}
+            alt="Github Icon"
+          />
+          <span>Star the github.</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
+
+<style lang="postcss">
+  .wrap {
+    @apply w-full shadow-md bg-gray/10;
+  }
+  nav {
+    @apply p-2 h-16 m-auto max-w-4xl flex items-center justify-between;
+  }
+
+  .github_icon {
+    @apply h-8;
+  }
+
+  h2 {
+    @apply text-secondary/80;
+  }
+
+  a.github_img {
+    @apply flex flex-col items-center text-xs;
+  }
+  a.github_img > span {
+    @apply text-gray/70;
+  }
+</style>
