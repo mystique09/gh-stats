edda4b3e58799fa6d2705ea5f29d7313426e94b8
mystique09 <knightkrypto7@gmail.com>
2022-08-05 09:20:24 +0800

    Revert "submodule"
    This reverts commit 1336f6865263285c194e348b2e1c8d2f71affa7b.
    
    Conflicts:
    	.gitmodules

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //    "./index.html",
+    "./src/**/*.{svelte,html,js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
